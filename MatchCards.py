62d48287a751ee0680a29f8eb29fb6de5b100bbc
Charlie <charlie30785@gmail.com>
2025-09-06 05:20:26 +0800

    fix best label & change reset and prepare function & add some note

--- MatchCards.py
+++ MatchCards.py
@@ -4,7 +4,7 @@ import random, os
 class MatchGame:
     def __init__(self,root):
         self.root = root
-        self.root.title("MatchGame")
+        self.root.title("Match Game")
         #Load card images
         self.images = {}
         self.back_img = None
@@ -33,49 +33,46 @@ class MatchGame:
         self.restart_button = tk.Button(self.root, text = "Restart Game",command = self.reset_game)
         self.restart_button.grid(row = 8, column = 0, columnspan = 10, pady = 10)
         #Game start
-        self.reset_game()
+        self.prepare()
     def load_images(self):
+        '''load cards image'''
         for card in [f'{i}' for i in range(1,21)]:
             path = os.path.join("images",f"{card}.png")
             img = Image.open(path).resize((60,90))
             self.images[card] = ImageTk.PhotoImage(img)
         back_path = os.path.join("images", "back.png")
         self.back_img = ImageTk.PhotoImage(Image.open(back_path).resize((60, 90)))
-    def reset_game(self):
+    def prepare(self):
+        '''shuffle and wait 10 second to show the cards'''
         self.cards = [f'{i}' for i in range(1,21)] * 2
         random.shuffle(self.cards)
-        self.buttons = []
-        self.lock = True
-        self.flipped.clear()
-        self.matched = [False for i in range(40)]
-        self.matchCount = 0
-        self.attempts = 0
-        self.time_elapset = 0
-        self.timer_running = False
         self.create_board()
-        self.status_label.config(text = "Time : 0s | Attempts : 0")
-        self.finish_label.config(text = "Remember The Cards")
         self.root.after(10000, self.start)
     def create_board(self):
+        '''create the button and show cards'''
         for i, card in enumerate(self.cards):
             btn = tk.Button(self.root, image = self.images[self.cards[i]],command = lambda idx = i : self.flip(idx))
             btn.grid(row = i // 10, column = i % 10, padx = 5, pady = 5)
             self.buttons.append(btn)
     def start(self):
+        '''get ready to start'''
         self.hide_cards()
         self.finish_label.config(text = "")
         self.lock = False
         self.timer_running = True
         self.update_timer()        
     def hide_cards(self):
+        '''hide all cards'''
         for btn in self.buttons:
             btn.config(image=self.back_img)
     def update_timer(self):
+        '''update timer every second'''
         if self.timer_running:
             self.time_elapset += 1
             self.status_label.config(text = f"Time : {self.time_elapset}s | Attempts : {self.attempts}")
             self.root.after(1000,self.update_timer)
     def flip(self, idx):
+        '''handle card flip'''
         if self.lock or idx in self.flipped or self.matched[idx]:
             return
         self.buttons[idx].config(image = self.images[self.cards[idx]])
@@ -86,6 +83,7 @@ class MatchGame:
             self.root.after(1000,self.check_match)
 
     def check_match(self):
+        '''check if two flipped cards matched or not and check end'''
         if(self.cards[self.flipped[0]] == self.cards[self.flipped[1]]):
             self.matched[self.flipped[0]] = True
             self.matched[self.flipped[1]] = True
@@ -100,9 +98,24 @@ class MatchGame:
             if self.best_time is None or self.time_elapset < self.best_time:
                 self.best_time = self.time_elapset
                 self.best_attempts = self.attempts
+            if self.time_elapset == self.best_time and self.attempts < self.best_attempts:
+                self.best_attempts = self.attempts
             self.status_label.config(text=f"Finished Time : {self.time_elapset}s | Attempts : {self.attempts}")
-            self.best_label.config(text=f"Best Time : {self.time_elapset}s | Attempts : {self.attempts}")
+            self.best_label.config(text=f"Best Time : {self.best_time}s | Attempts : {self.best_attempts}")
             self.finish_label.config(text="Congratulations!")
+    def reset_game(self):
+        '''reset the game and go prepare'''
+        self.buttons = []
+        self.lock = True
+        self.flipped.clear()
+        self.matched = [False for i in range(40)]
+        self.matchCount = 0
+        self.attempts = 0
+        self.time_elapset = 0
+        self.timer_running = False
+        self.status_label.config(text = "Time : 0s | Attempts : 0")
+        self.finish_label.config(text = "Remember The Cards")
+        self.prepare()
 if __name__ == "__main__":
     root = tk.Tk()
     game = MatchGame(root)
